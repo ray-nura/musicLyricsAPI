148a2cc5f4b839b14d2a6a8cbe212b486761715c
Nur <raynura182@gmail.com>
2022-02-01 00:59:16 -0800

    screenShot img added

---
+++ musicIMG.js
@@ -0,0 +1,88 @@
+export const musicIMG = function () {
+    return [
+        {
+            "id": "01",
+            "name": "Jabbar",
+            "title": "Yineleme",
+            "imgURL": "https://cdns-images.dzcdn.net/images/cover/5de68b62648673c6e345abdd9c60080c/350x350.jpg"
+
+        },  
+        {
+            "id": "02",
+            "name": "Adele Hello",
+            "title": "Hello",
+            "imgURL": "https://wallpaperaccess.com/full/452829.jpg"
+
+        },  
+        {
+            "id": "03",
+            "name": "Ariana Grande",
+            "title": "God is a woman",
+            "imgURL": "https://picstatio.com/large/3efd80/Ariana-Grande-famous-actress.jpg"
+
+        },  
+        {
+            "id": "04",
+            "name": "Halsey",
+            "title": "Bad at Love",
+            "imgURL": "https://wallpapersmug.com/download/1024x768/8b288c/singer-famous-halsey.jpg"
+
+        },  
+        {
+            "id": "05",
+            "name": "Metallica",
+            "title": "The Unforgiven",
+            "imgURL": "https://townsquare.media/site/366/files/2021/12/attachment-metallica_takeover_amazon_header.jpg"
+
+        },  
+        {
+            "id": "06",
+            "name": "Beyonce",
+            "title": "Halo",
+            "imgURL": "https://wallpaperaccess.com/full/452748.jpg"
+
+        },  
+        {
+            "id": "07",
+            "name": "Demi",
+            "title": "Heart Attack",
+            "imgURL": "https://www.desktopbackground.org/download/o/2011/01/02/135951_demi-lovato-hot-hd-wallpapers-famous-singer-demi-lovato-photos_1920x1080_h.jpg"
+
+        },  
+        {
+            "id": "08",
+            "name": "Black Eyed Peas",
+            "title": "I Gotta Feeling",
+            "imgURL": "https://cdn2.thelineofbestfit.com/images/made/images/remote/https_cdn2.thelineofbestfit.com/media/2014/Black_Eyed_Peas_3_by_Nabil_Elderkin_1290_859.jpg"
+
+        },  
+        {
+            "id": "09",
+            "name": "BTS",
+            "title": "Life Goes On",
+            "imgURL": "https://talentrecap.com/wp-content/uploads/2020/09/bts-became-popular-in-America.jpg"
+
+        },  
+        {
+            "id": "10",
+            "name": "Sting",
+            "title": "Duets",
+            "imgURL": "https://www.star.wine/wp-content/uploads/2021/04/Star-Wine-Sting-Sting-Duets-exklusive_signed.jpg"
+
+        },  
+        {
+            "id": "11",
+            "name": "Jony",
+            "title": "Comet",
+            "imgURL": "https://i.ytimg.com/vi/C7aYlveb2qE/hqdefault.jpg"
+
+        }, 
+        {
+            "id": "13",
+            "name": "Jazz ",
+            "title": "I Got The Blues",
+            "imgURL": "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2012/05/21/Style/Images/KDJAZZ27.jpg?t=20170517"
+
+        }, 
+        
+    ]}
--- script.js
+++ script.js
@@ -1,49 +1,59 @@
 import { firstPageData } from "./firstPageData.js";
+import { musicIMG } from "./musicIMG.js";
 
 const firstPageDataArray = firstPageData();
+const musicIMGArray = musicIMG();
 const apiURL = "https://api.lyrics.ovh";
-const serchSection = document.querySelector(".search")
+const searchVal = document.querySelector("#searchInput")
 const firstPageSection = document.querySelector(".first-page");
 const firstPageH1 = document.querySelector("#first-page");
 const resultSection = document.querySelector(".result");
-const resultH1 = document.querySelector("#result");
 const btnPrev = document.querySelector("#prevBtn");
 const btnNext = document.querySelector("#nextBtn");
 const btnNextPrevSection = document.querySelector(".btn");
-const oneSongSection = document.querySelector(".one-song")
 const showLyricsSection = document.querySelector("#showLyrics")
-// need to wrap in function
-const firstPageCard = document.createElement("div");
-firstPageCard.className = "flex";
-firstPageCard.innerHTML = `${firstPageDataArray.map(el => `
-        <div class="card" style="background-image:url('${el.imgURL}');">
-            <h3 class="artistName">${el.name}</h3>
-            </div>
-        `).join('')}`
-firstPageSection.appendChild(firstPageCard)
+
+//  first page function
+function firstStart(impData) {
+    const firstPageCard = document.createElement("div");
+    firstPageCard.className = "flex";
+    firstPageCard.innerHTML = `${impData.map(el => `
+    <div class="card" style="background-image:url('${el.imgURL}');">
+    <h3 class="artistName">${el.name}</h3>
+    </div> `).join('')}`
+    firstPageSection.appendChild(firstPageCard)
+}
+
+// --------call start function !!! -----------------
+// firstStart(musicIMGArray);
+firstStart(firstPageDataArray);
 
 // get search value
 document.querySelector("#searchBtn").addEventListener("click", e => {
-    e.preventDefault();
-    const searchVal = document.querySelector("#searchInput").value;
-    console.log(searchVal)
-    beginSearch(searchVal);
+    // e.preventDefault();
+    beginSearch(searchVal.value);
+})
+searchVal.addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        beginSearch(searchVal.value);
+    }
 })
 // get search value from img category
 firstPageSection.addEventListener("click", e => {
     const clickedElement = e.target.innerText;
-    console.log(clickedElement)
     beginSearch(clickedElement);
 })
 // Search function/  greate searched Array from Data
 async function beginSearch(searchValue) {
     const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
     const data = await searchResult.json();
-    const data1 = data.data.slice(0, 4)
-    const lengthData = data.data.length
+    const data1 = data.data.slice(0, 4);
+    const lengthData = data.data.length;
     console.log(data)
     let num = 0;
     displayData(data1, num, lengthData, searchValue);
+    // display Next button
     if (data.data.length > 5) {
         btnNextPrevSection.style.display = "block";
         btnPrev.style.display = 'none';
@@ -69,22 +79,17 @@ async function beginSearch(searchValue) {
             btnPrev.style.display = 'none';
         }
     })
-
 }
-// display Next button
-
 // Display Searched Array  
 function displayData(data, num, lengthData, searchValue) {
     firstPageSection.style.display = "none";
     firstPageH1.style.display = "none";
-    // resultH1.style.display = "block";
-    // btnNextPrevSection.style.display = "block";
     resultSection.innerHTML = ' ';
     showLyricsSection.innerHTML = ' ';
     const resultCard = document.createElement("div");
     resultCard.innerHTML = ` 
     <h1>All Songs with the word: "${searchValue}"</h1>
-    <h1>${num} - ${num+4} out of ${lengthData} songs</h1><div class="container">
+    <h1>${num} - ${num + 4} out of ${lengthData} songs</h1><div class="container">
     ${data.map(el => `
     <div class="slide" style="background-image:url('${el.artist.picture_big}');">
         <div class="info">
@@ -93,19 +98,18 @@ function displayData(data, num, lengthData, searchValue) {
             <h3 class="titleName">Albom: ${el.album.title}</h3>
         </div>
         <div class="middle">
-            <div class="bar bar3"></div>
-            <div class="bar bar1"></div>
-            <div class="bar bar2"></div>
-            <div class="bar bar5"></div>
-            <div class="bar bar4"></div>
-            <div class="bar bar7"></div>
-            <div class="bar bar8"></div>
-            <div class="bar bar6"></div>
+            <div class="bar bar3"></div> <div class="bar bar2"></div>
+            <div class="bar bar5"></div><div class="bar bar1"></div>
+            <div class="bar bar4"></div><div class="bar bar8"></div>
+            <div class="bar bar7"></div><div class="bar bar6"></div>
+            <div class="bar bar1"></div><div class="bar bar3"></div>
+            <div class="bar bar5"></div><div class="bar bar2"></div>
+            <div class="bar bar8"></div><div class="bar bar4"></div>
+            <div class="bar bar7"></div><div class="bar bar6"></div>
         </div>
         <div class="playInfo">
             <audio controls>
-                <source src="${el.preview}" type="audio/mpeg">
-                Your browser does not support the audio element.
+                <source src="${el.preview}" type="audio/mpeg">Your browser does not support the audio element.
             </audio>
             <button class="getlyricsBtn" data-artist="${el.artist.name}" data-songtitle="${el.title}">Get lyrics</button>
         </div>
@@ -114,7 +118,7 @@ function displayData(data, num, lengthData, searchValue) {
     resultSection.appendChild(resultCard);
     // adding ACTIVE class to slide
     const slides = document.querySelectorAll('.slide');
-    slides[1].classList.add('active');
+    slides[0].classList.add('active');
     for (const slide of slides) {
         slide.addEventListener('click', () => {
             clearActiveClasses()
@@ -127,7 +131,6 @@ function displayData(data, num, lengthData, searchValue) {
             slide.classList.remove('active');
         })
     }
-
     // click btn "get Lyrics"
     const getlyrics = document.querySelectorAll(".getlyricsBtn");
     for (const btn of getlyrics) {
@@ -151,8 +154,7 @@ function displayData(data, num, lengthData, searchValue) {
             const lyrics1 = data.lyrics.replace(/(\n\n\n\n)/g, '<br><br>');
             const lyrics = lyrics1.replace(/(\r\n|\r|\n)/g, '</p> <p>');
             lyricsS.innerHTML = `
-            <span class="closeBtn">&times;</span><p>
-            ${lyrics}</p>`
+            <span class="closeBtn">&times;</span><p>${lyrics}</p>`
             showLyricsSection.appendChild(lyricsS);
         } else {
             showLyricsSection.innerHTML = "";
@@ -161,11 +163,8 @@ function displayData(data, num, lengthData, searchValue) {
             <h1 style="color:red;" >Lyrics Not Found</h1>`;
             showLyricsSection.appendChild(lyricsS);
         }
-        document.querySelector(".closeBtn").addEventListener("click", ()=>{
-        showLyricsSection.style.display = "none";
-
+        document.querySelector(".closeBtn").addEventListener("click", () => {
+            showLyricsSection.style.display = "none";
         })
     }
 }
-
-
